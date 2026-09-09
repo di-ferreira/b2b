deaeea487a4abf9784ee595afd4ead0707a5b4ff
Diego Ferreira <diferreira.program@gmail.com>
2026-09-09 10:39:48 -0300

    fix(auth): make logout a server component that redirects to /auth

--- src/app/app/logout/page.tsx
+++ src/app/app/logout/page.tsx
@@ -1,10 +1,10 @@
-'use client';
 import { removeCookie } from '@/app/actions';
+import { redirect } from 'next/navigation';
 
 export default async function Logout() {
   await removeCookie('token_b2b');
   await removeCookie('user_b2b');
+  await removeCookie('CIC');
 
-  //   redirect('/auth');
+  redirect('/auth');
 }
-

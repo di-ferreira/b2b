fb5ec7f7f25f8ca670690c0623a9bc08c5dc9588
Diego Ferreira <diferreira.program@gmail.com>
2026-09-23 11:39:59 -0300

    style(products): add top padding between navbar and table

--- src/app/app/products/page.tsx
+++ src/app/app/products/page.tsx
@@ -3,7 +3,7 @@ import DataTableProducts from '@/components/products/DataTable';
 const Products = () => {
   return (
     <div className='flex flex-col w-full h-[calc(100vh-74px)] overflow-x-hidden overflow-y-auto'>
-      <section className='flex flex-col w-full h-full overflow-x-hidden overflow-y-scroll'>
+      <section className='flex flex-col w-full h-full pt-4 overflow-x-hidden overflow-y-scroll'>
         <DataTableProducts />
       </section>
     </div>
